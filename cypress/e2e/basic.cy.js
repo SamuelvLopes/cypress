6dcec1d46518ee18bc25641ddc50c63a0a26a05f
Samuel Lopes <samuelvitorprofissional@gmail.com>
2024-03-02 21:53:08 -0300

    visitando pagina e verificando elemento

---
+++ cypress/e2e/basic.cy.js
@@ -0,0 +1,24 @@
+
+describe('Cypress basic',()=>{
+
+    it('should visit a page and assert title',()=>{
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html');
+        
+        cy.title()
+            .should('be.equal','Campo de Treinamento')
+            .and('contain','ampo');
+
+        
+        
+    });
+
+    it('should find a element in page',()=>{
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html');
+        
+        cy.get('#buttonSimple').click().should('have.value','Obrigado!');
+
+
+    });
+});
